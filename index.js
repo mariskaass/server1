0ed5555eb6827c3854275334cc9f92c4791ba28b
mariskaass <mariska.boelhouwers@hva.nl>
2025-02-24 12:49:38 +0100

    dfgh

--- index.js
+++ index.js
@@ -2,15 +2,39 @@ console.log("hallo world");
 const express = require('express');
 const app = express();
 
+app.set('view engine', 'ejs');
+app.set('views', 'view');
+
+app.use(express.urlencoded({extended: true}));
+
+app.use('/static', express.static('static'));
+
 app.get('/', onhome);
+app.post('/add_name', add_name)
 app.get('/AAAH', onAAAH);
+app.get('/B', onB);
+
+
 
 app.listen(8000);
 
-function onhome(req, res) {
-    res.send('<h1>kaas</h1>');
+function onhome(req, res) { 
+    // let stuff = {
+    //     naam: 'henk',
+    //     kaas: 'ja'
+    // }
+    res.render('index.ejs');
+}
+
+function add_name(req, res) {
+    
 }
 
 function onAAAH(req, res) {
-    res.send('<h1>AAAAAAAAAAAAHHHHHH</h1>')
+    res.render('deel.ejs');
+}
+
+function onB(req, res) {
+    res.send('<link rel="stylesheet" href="static/css/style.css"> <h1>B</h1>  <h2>C</h2>');
+   
 }
